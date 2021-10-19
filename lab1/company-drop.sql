56f8b9c093e6a6e93d6ab8c4101f066cdfc3262d
acyanbird <2534930703@qq.com>
2021-10-19 16:05:53 +0100

    first

---
+++ lab1/company-drop.sql
@@ -0,0 +1,6 @@
+DROP TABLE employee CASCADE;
+DROP TABLE department CASCADE;
+DROP TABLE dept_locations CASCADE;
+DROP TABLE project CASCADE;
+DROP TABLE works_on CASCADE;
+DROP TABLE dependent CASCADE;
